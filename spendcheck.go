121547423596824591a76a202866f20d4e3bc26b
Adam Eury <euryadam@gmail.com>
2023-12-25 00:32:59 -0500

    Add support for defining a spending plan and summarizing transactions.

--- spendcheck.go
+++ spendcheck.go
@@ -1,3 +1,118 @@
 package spendcheck
 
-// build me!
+import "errors"
+
+var (
+	// ErrInvalidExpenseAllocations is returned when the sum of the expense allocations is not 1.0.
+	ErrInvalidExpenseAllocations = errors.New("invalid expense allocations")
+)
+
+// TransactionType is the type of transaction.
+type TransactionType int
+
+const (
+	FixedCost TransactionType = iota
+	Investment
+	Savings
+	Discretionary
+)
+
+// Transaction is a transaction.
+type Transaction struct {
+	Amount float64
+	Type   TransactionType
+}
+
+// Plan is a budget plan.
+type Plan struct {
+	NetIncome     float64
+	FixedCosts    float64
+	Investments   float64
+	Savings       float64
+	Discretionary float64
+	Transactions  []Transaction
+}
+
+// NewPlan creates a new Plan.
+func NewPlan(netIncome, fixedCosts, investments, savings, discretionary float64) (*Plan, error) {
+	totalAllocation := fixedCosts + investments + savings + discretionary
+	if totalAllocation != 1.0 {
+		return nil, ErrInvalidExpenseAllocations
+	}
+	return &Plan{
+		NetIncome:     netIncome,
+		FixedCosts:    fixedCosts,
+		Investments:   investments,
+		Savings:       savings,
+		Discretionary: discretionary,
+		Transactions:  []Transaction{},
+	}, nil
+}
+
+// AddFixedCost adds a fixed cost transaction to the plan.
+func (p *Plan) AddFixedCost(amount float64) {
+	t := Transaction{
+		Amount: amount,
+		Type:   FixedCost,
+	}
+	p.Transactions = append(p.Transactions, t)
+}
+
+// AddInvestment adds an investment transaction to the plan.
+func (p *Plan) AddInvestment(amount float64) {
+	t := Transaction{
+		Amount: amount,
+		Type:   Investment,
+	}
+	p.Transactions = append(p.Transactions, t)
+}
+
+// AddSavings adds a savings transaction to the plan.
+func (p *Plan) AddSavings(amount float64) {
+	t := Transaction{
+		Amount: amount,
+		Type:   Savings,
+	}
+	p.Transactions = append(p.Transactions, t)
+}
+
+// AddDiscretionary adds a discretionary transaction to the plan.
+func (p *Plan) AddDiscretionary(amount float64) {
+	t := Transaction{
+		Amount: amount,
+		Type:   Discretionary,
+	}
+	p.Transactions = append(p.Transactions, t)
+}
+
+// SpendingSummary is a summary of spending.
+type SpendingSummary struct {
+	FixedCosts    float64
+	Investments   float64
+	Savings       float64
+	Discretionary float64
+}
+
+// SummarizeSpending computes the spending among each category
+// as a percentage of net income.
+func (p *Plan) SummarizeSpending() SpendingSummary {
+	var fixedCosts, investments, savings, discretionary float64
+	for _, t := range p.Transactions {
+		switch t.Type {
+		case FixedCost:
+			fixedCosts += t.Amount
+		case Investment:
+			investments += t.Amount
+		case Savings:
+			savings += t.Amount
+		case Discretionary:
+			discretionary += t.Amount
+		}
+	}
+	return SpendingSummary{
+		FixedCosts:    fixedCosts / p.NetIncome,
+		Investments:   investments / p.NetIncome,
+		Savings:       savings / p.NetIncome,
+		Discretionary: discretionary / p.NetIncome,
+	}
+}
--- spendcheck_test.go
+++ spendcheck_test.go
@@ -1,3 +1,122 @@
 package spendcheck_test
 
-// write some tests!
+import (
+	"spendcheck"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestNewPlan(t *testing.T) {
+	t.Parallel()
+	wantPlan := spendcheck.Plan{
+		NetIncome:     1000.0,
+		FixedCosts:    0.5,
+		Investments:   0.1,
+		Savings:       0.1,
+		Discretionary: 0.3,
+		Transactions:  []spendcheck.Transaction{},
+	}
+	p, err := spendcheck.NewPlan(1000.0, 0.5, 0.1, 0.1, 0.3)
+	if err != nil {
+		t.Fatal("didn't expect an error. got: ", err)
+	}
+	if !cmp.Equal(wantPlan, *p) {
+		t.Error(cmp.Diff(wantPlan, *p))
+	}
+}
+
+func TestNewPlan_ReturnsErrorForInvalidExpenseAllocations(t *testing.T) {
+	t.Parallel()
+	_, err := spendcheck.NewPlan(1000.0, 0.5, 0.1, 0.1, 0.4)
+	if err == nil {
+		t.Error("want error, got nil")
+	}
+}
+
+func TestAddFixedCost(t *testing.T) {
+	t.Parallel()
+	p, err := spendcheck.NewPlan(1000.0, 0.5, 0.1, 0.1, 0.3)
+	if err != nil {
+		t.Fatal("didn't expect an error. got: ", err)
+	}
+
+	p.AddFixedCost(100.0)
+	wantTransactions := []spendcheck.Transaction{
+		{Amount: 100.0, Type: spendcheck.FixedCost},
+	}
+	if !cmp.Equal(wantTransactions, p.Transactions) {
+		t.Error(cmp.Diff(wantTransactions, p.Transactions))
+	}
+}
+
+func TestAddInvestment(t *testing.T) {
+	t.Parallel()
+	p, err := spendcheck.NewPlan(1000.0, 0.5, 0.1, 0.1, 0.3)
+	if err != nil {
+		t.Fatal("didn't expect an error. got: ", err)
+	}
+
+	p.AddInvestment(100.0)
+	wantTransactions := []spendcheck.Transaction{
+		{Amount: 100.0, Type: spendcheck.Investment},
+	}
+	if !cmp.Equal(wantTransactions, p.Transactions) {
+		t.Error(cmp.Diff(wantTransactions, p.Transactions))
+	}
+}
+
+func TestAddSavings(t *testing.T) {
+	t.Parallel()
+	p, err := spendcheck.NewPlan(1000.0, 0.5, 0.1, 0.1, 0.3)
+	if err != nil {
+		t.Fatal("didn't expect an error. got: ", err)
+	}
+
+	p.AddSavings(100.0)
+	wantTransactions := []spendcheck.Transaction{
+		{Amount: 100.0, Type: spendcheck.Savings},
+	}
+	if !cmp.Equal(wantTransactions, p.Transactions) {
+		t.Error(cmp.Diff(wantTransactions, p.Transactions))
+	}
+}
+
+func TestAddDiscretionary(t *testing.T) {
+	t.Parallel()
+	p, err := spendcheck.NewPlan(1000.0, 0.5, 0.1, 0.1, 0.3)
+	if err != nil {
+		t.Fatal("didn't expect an error. got: ", err)
+	}
+
+	p.AddDiscretionary(300.0)
+	wantTransactions := []spendcheck.Transaction{
+		{Amount: 300.0, Type: spendcheck.Discretionary},
+	}
+	if !cmp.Equal(wantTransactions, p.Transactions) {
+		t.Error(cmp.Diff(wantTransactions, p.Transactions))
+	}
+}
+
+func TestSummarizeSpending(t *testing.T) {
+	t.Parallel()
+	p, err := spendcheck.NewPlan(1000.0, 0.5, 0.1, 0.1, 0.3)
+	if err != nil {
+		t.Fatal("didn't expect an error. got: ", err)
+	}
+
+	p.AddFixedCost(500.0)
+	p.AddInvestment(100.0)
+	p.AddSavings(100.0)
+	p.AddDiscretionary(250.0)
+	summary := p.SummarizeSpending()
+	wantSummary := spendcheck.SpendingSummary{
+		FixedCosts:    .5,
+		Investments:   .1,
+		Savings:       .1,
+		Discretionary: .25,
+	}
+	if !cmp.Equal(wantSummary, summary) {
+		t.Error(cmp.Diff(wantSummary, summary))
+	}
+}
